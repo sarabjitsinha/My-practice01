dcdc998e7cdc2ac80b5102d8eb990edc5089bd96
sarabjitsinha <sinhasarabjit@gmail.com>
2024-10-12 21:17:06 +0530

    adding to new1

---
+++ js1.js
@@ -0,0 +1,21 @@
+let a=10;
+let b=20;
+console.log(a+b);
+
+//let i=0;
+//let k=9;
+
+for(i=1;i<=10;i++)
+{
+    console.log('\n');
+    
+for(k=0;k<i;k++)
+    {
+    console.log('*');
+  //  console.log('\n');
+    
+  }
+}
+
+
+
