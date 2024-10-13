ff5a0df50bb37c66d1b2fddb3262ce8b781e2f28
sarabjitsinha <sinhasarabjit@gmail.com>
2024-10-13 15:29:32 +0530

    adding new js file

--- js1.js
+++ js1.js
@@ -5,17 +5,49 @@ console.log(a+b);
 //let i=0;
 //let k=9;
 
-for(i=1;i<=10;i++)
-{
-    console.log('\n');
+// for(i=1;i<=10;i++)
+// {
+//     console.log('\n');
     
-for(k=0;k<i;k++)
-    {
-    console.log('*');
-  //  console.log('\n');
+// for(k=0;k<i;k++)
+//     {
+//     console.log('*');
+//   //  console.log('\n');
     
-  }
-}
+//   }
+// }
 
+let c='20';
+let d=20;
+let result=c-d;
+console.log("Using minus operator on string and number",result);
 
+ c='20';
+ d=20;
+ result=c+d;
+console.log("Using plus operator on string and number",result);
 
+ c='20';
+ d=20;
+ result=c==d;
+console.log("Using comparision operator",result);
+
+ c='20';
+ d=20;
+ result=c===d;
+console.log("Using strict eqality",result);
+
+c='Sarabjit';
+d=20;
+result=c-d;
+console.log("Using minus operator on string(character) and number",result);
+console.log(typeof(result));
+
+c='Sarabjit';
+d=20;
+result=c!==d;
+console.log("Using strict inequality",result);
+console.log(typeof(result));
+
+let t=Math.floor(Math.random()*900+100)
+console.log(t);
---
+++ js1_1.js
@@ -0,0 +1,15 @@
+let i=0;
+
+
+for(i=1;i<=10;i++)
+{
+    //console.log('\n');
+    let k=0;
+    for(k=0;k<i;k++)
+    {
+    
+    console.log('*');
+  //console.log('\n');
+    
+  }
+}
